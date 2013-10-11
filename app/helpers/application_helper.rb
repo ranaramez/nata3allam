88230c677d6233cd995826ce68c963676b162f8b
ranaramez <rana.ramez@gmail.com>
2013-10-11 21:15:09 +0200

    navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def nav_link(link_text, link_path, classes)
+    class_name = current_page?(link_path) ? 'active ' : ''
+
+    content_tag(:li, class: class_name) do
+      link_to link_path, class: classes do
+        content_tag(:i, "", nil).concat(link_text)
+      end
+    end
+  end
 end
