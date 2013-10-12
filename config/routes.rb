ad550c805b5e78f53012125870b3573e54b07da8
ranaramez <rana.ramez@gmail.com>
2013-10-12 03:09:14 +0200

    schedule styling haml

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Nata3allam::Application.routes.draw do
   
   resources :admin_dashboard, only: :index
 
-  resources :n_class
+  resources :n_class do
+    get :schedule
+    put :course_week
+  end
 
   match '/class_schedule', :to => 'n_class#schedule'
     
