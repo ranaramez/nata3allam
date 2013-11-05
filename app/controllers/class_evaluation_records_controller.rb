3e5fe8d41ff7e111be8f7bab86070f157632434e
nazlysabbour <nazly.sabbour@gmail.com>
2013-11-06 00:41:51 +0200

    changing evaluation

--- app/controllers/class_evaluation_records_controller.rb
+++ app/controllers/class_evaluation_records_controller.rb
@@ -1,49 +1,34 @@
 class ClassEvaluationRecordsController < ApplicationController
 
   def edit
-    if params[:month].present?
-      @month = params[:month].to_i
-    else
-      @month = Date.today.month
-    end
-    @month = 1 if @month < 1
-    @month = (@month % 12) if @month > 12
-
+    lesson_id = params[:lesson_id]
     @class_subject ||= ClassSubject.where(_id: params[:class_subject_id]).first
+    @lesson = @class_subject.lessons.where(_id: lesson_id).first
     @students = @class_subject.n_class.students
     @n_class = @class_subject.n_class
     @teacher = @n_class.teacher_name    
-    @evaluation_record = @class_subject.class_evaluation_records.all.first
-    @evaluation_record ||= ClassEvaluationRecord.template @class_subject, @students
-    @lessons = @class_subject.class_schedule_entries.where(month: @month).map(&:lesson)
-
+    @evaluation_record = @class_subject.class_evaluation_records.where('lesson._id' => Moped::BSON::ObjectId(lesson_id)).first
+    @evaluation_record ||= ClassEvaluationRecord.template @class_subject, @students, @lesson
   end
 
   def create
+    puts params, params[:lesson_id] 
+    debugger
+    lesson_id = params[:lesson_id]
     id = params[:class_evaluation_record][:_id]
     if id.present?
       record = ClassEvaluationRecord.where(_id: id).first
     end
     record ||= ClassEvaluationRecord.new
+    class_subject_id = params[:class_evaluation_record][:class_subject_id]
+    @class_subject = ClassSubject.where(_id: class_subject_id).first
+    lesson = @class_subject.lessons.where(_id: lesson_id).first
+    puts lesson.description, lesson.attributes
+    record.lesson = lesson
     record.update_attributes params[:class_evaluation_record]
-    @class_subject = record.class_subject
-    lessons = @class_subject.class_schedule_entries.where(month: params[:month].to_i).map(&:lesson)
+    record.save!
 
-    record.evaluation_records.each_with_index do |evaluation_record, i|
-      edited_grades = evaluation_record.grades.where("lesson_id" => {"$ne" => nil})
-      puts edited_grades.map(&:as_document)
-      grades =  []
-      edited_grades.each_with_index do |grade, j|
-        lesson_id = params[:class_evaluation_record][:evaluation_records_attributes][i.to_s][:grades_attributes][j.to_s]["lesson_id"]
-        grade.lesson = @class_subject.lessons.where(_id: lesson_id).first
-        grade.set "lesson_id", nil
-        grades << grade
-      end
-      evaluation_record.grades += grades
-      evaluation_record.grades = evaluation_record.grades.uniq
-      evaluation_record.save!
-    end
-    redirect_to edit_class_evaluation_record_path( id: id, class_subject_id: @class_subject._id, month: params[:month])
+    redirect_to edit_class_evaluation_record_path( id: id, class_subject_id: @class_subject._id, lesson_id: lesson_id)
   end
 
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -6,6 +6,7 @@ class Lesson
   field :performance_metrics, :type => Hash #hash /just metrics and constant grading system
   embedded_in :class_subject, :class_name => "ClassSubject", :inverse_of => "lessons"
   embedded_in :subject, :class_name => "Subject", :inverse_of => "lessons"
-  embedded_in :grade, :class_name => "Grade", :inverse_of => "lesson"
+  embedded_in :evaluation_record, :class_name => "EvaluationRecord", :inverse_of => "lesson"
+  embedded_in :class_evaluation_record, :class_name => "ClassEvaluationRecord", :inverse_of => "lesson"
   embedded_in :class_schedule_entry, :class_name => "ClassScheduleEntry", :inverse_of => "lesson"
 end
