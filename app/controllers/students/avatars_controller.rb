d1844b49c4b7630b63478ad8c90a80756e5afa02
nazlysabbour <nazly.sabbour@gmail.com>
2013-12-06 23:44:21 +0200

    upload photo for student

---
+++ app/controllers/students/avatars_controller.rb
@@ -0,0 +1,19 @@
+class Students::AvatarsController < ApplicationController
+
+  def new
+    @avatar = Avatar.new
+    @student_id = params[:student_id]
+    @student = Student.where(_id: @student_id).first
+  end
+
+  def create
+  	@person = Person.where(_id:params[:avatar][:person_id]).first
+  	@person.avatar = nil
+  	@person.save!
+    avatar = Avatar.new(params[:avatar])
+    puts params
+    avatar.save!
+    redirect_to student_path(avatar.person)
+  end
+
+end
