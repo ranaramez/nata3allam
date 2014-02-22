1758502d0ec310c55cf780e97f178da4afb33d96
Nazly Sabbour <nazly.sabbour@gmail.com>
2014-02-22 15:56:46 +0200

    fix glitch in search outside home

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,6 +1,6 @@
 $(function() {
   $( "#main-search" ).autocomplete({
-    source: "search/autocomplete",
+    source: "/search/autocomplete",
     minLength: 2,
     select: function( event, ui ) {
      window.location = '/students/'+ui.item.id
