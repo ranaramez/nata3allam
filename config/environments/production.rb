84638915ff38a3cb4a177320839f1fa56b367e83
Nazly Sabbour <nazly.sabbour@gmail.com>
2014-08-17 18:26:05 +0200

    adding files to config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Nata3allam::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( application.js application.css admin.css admin.js application-rtl.css style-default.css uniform.default.css application-dashboard-rtl.css application-dashboard.js system/html5shiv.js excanvas/excanvas.js json2.js style.css flexslider.css keyframes.css grid.css meanmenu.css spinner.css rtl.css application_guest.js)
+  config.assets.precompile += %w( application.js application.css admin.css admin.js application-rtl.css style-default.css uniform.default.css application-dashboard-rtl.css application-dashboard.js system/html5shiv.js excanvas/excanvas.js json2.js style.css flexslider.css keyframes.css grid.css meanmenu.css spinner.css rtl.css application_guest.js font-awesome.css font-awesome-ie7.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
